9cb4116de2c33300f3aa2248a677b84d4cac29d4
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-10-13 15:47:42 -0400

    Camera

--- src/gameplay/mod.rs
+++ src/gameplay/mod.rs
@@ -9,9 +9,26 @@ pub async fn gameplay() {
 	loop {
 		clear_background(RED);
 
+
+        set_camera(&Camera2D {
+			zoom: vec2(1. / camera_scale(), screen_width() / screen_height() / camera_scale()),
+            target: player.pos,
+            ..Default::default()
+        });
+
 		player.update();
-        draw_circle(player.pos.x, player.pos.y, 15.0, YELLOW);
+        draw_circle(player.pos.x, player.pos.y, 15.0, YELLOW); // Player
+
+		draw_circle(0.0, 0.0, 15.0, GREEN); // Test Object
+
+		set_default_camera();
+ 
+		draw_text("Test", 32.0, 64.0, camera_scale() / 10., BLACK); // Test UI
 
 		next_frame().await;
 	}
+}
+
+fn camera_scale() -> f32 {
+	return screen_width() / screen_height() * 512.
 }
