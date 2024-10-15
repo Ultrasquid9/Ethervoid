f9f31fac56e49cf6f58596626e657e8b077f8703
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-10-15 14:44:08 -0400

    Allow Multiple Enemies and the Killing Of Enemies

--- src/gameplay/mod.rs
+++ src/gameplay/mod.rs
@@ -15,11 +15,10 @@ pub struct Entity {
 
 pub async fn gameplay() -> State {
 	let mut player = Player::new(); // Creates a player
-	let mut enemy = Enemy::new(); // Creates an enemy
+	let mut enemies = vec![Enemy::new()]; // Creates a list of enemies
 
-	loop {
-		// Draws the background
-		clear_background(RED);
+	loop {		
+		clear_background(RED); // Draws the background
 
 		// Creates a camera targetting the player
         set_camera(&Camera2D {
@@ -30,23 +29,29 @@ pub async fn gameplay() -> State {
 
 		// Updates the player and all enemies
 		player.update();
-		enemy.update(&mut player);
 
-		if is_key_down(get_keycode(&player.config, "Attack")) {
-			if enemy.stats.pos.distance(player.stats.pos) < 64.0 {
-				enemy.damage(1);
+		if enemies.len() > 0 {
+			update_enemies(&mut player, &mut enemies);
+
+			let enemies_to_kill = enemies_to_kill(&enemies);
+			enemies.retain(|_| *enemies_to_kill.iter().next().unwrap());
+		}
+
+		// Drawing the Player and enemies
+        draw_circle(player.stats.pos.x, player.stats.pos.y, 15.0, YELLOW); // Player
+		if enemies.len() > 0 {
+			for i in &enemies {
+				draw_circle(i.stats.pos.x, i.stats.pos.y, 15.0, GREEN); // Enemies
 			}
 		}
 
-		// The Player and enemies
-        draw_circle(player.stats.pos.x, player.stats.pos.y, 15.0, YELLOW); // Player
-		draw_circle(enemy.stats.pos.x, enemy.stats.pos.y, 15.0, GREEN); // Test Object
-
 		set_default_camera();
  
-		// The UI
+		// Drawing a temporary UI
 		draw_text(&format!("{}", player.stats.health), 32.0, 64.0, camera_scale() / 10., BLACK);
-		draw_text(&format!("{}", enemy.stats.health), 32.0, 128.0, camera_scale() / 10., BLACK);
+		if enemies.len() > 0 {
+			draw_text(&format!("{}", enemies[0].stats.health), 32.0, 128.0, camera_scale() / 10., BLACK);
+		}
 
 		// Quits the game
 		if is_key_down(get_keycode(&player.config, "Quit")) {
@@ -61,4 +66,26 @@ pub async fn gameplay() -> State {
 /// Gets the scale that the camera should be rendered at
 fn camera_scale() -> f32 {
 	return screen_width() / screen_height() * 512.
+}
+
+fn update_enemies(player: &mut Player, enemies: &mut Vec<Enemy>) {
+	for i in enemies {
+		i.update(player);
+
+		if is_key_down(get_keycode(&player.config, "Attack")) {
+			if i.stats.pos.distance(player.stats.pos) < 64.0 {
+				i.damage(1);
+			}
+		}
+	}
+}
+
+fn enemies_to_kill(enemies: &Vec<Enemy>) -> Vec<bool> {
+	let mut enemies_to_kill: Vec<bool> = Vec::new();
+
+	for i in enemies {
+		enemies_to_kill.push(!i.should_kill());
+	}
+
+	return enemies_to_kill;
 }
