c1f2a23b4bd1c38a483adcda9c71e9e8fa9ad449
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-11-05 16:16:53 -0500

    Serialize Vec2

--- src/gameplay/cores/map.rs
+++ src/gameplay/cores/map.rs
@@ -3,12 +3,12 @@ use std::{collections::HashMap, fs};
 use macroquad::math::Vec2;
 use serde::Deserialize;
 
-use super::{enemytype::{get_enemytypes, EnemyType}, gen_name, get_files, Point};
+use super::{enemytype::{get_enemytypes, EnemyType}, gen_name, get_files};
 
 #[derive(Deserialize)]
 struct MapBuilder {
-	pub points: Vec<Point>,
-	pub enemies: Vec<(String, Point)>
+	pub points: Vec<Vec2>,
+	pub enemies: Vec<(String, Vec2)>
 }
 
 pub struct Map {
@@ -25,15 +25,12 @@ impl MapBuilder {
 		let enemytypes = get_enemytypes();
 
 		Map {
-			points: self.points
-				.iter()
-				.map(|point| point.to_vec2())
-				.collect(),
+			points: self.points,
 			enemies: self.enemies
 				.iter()
 				.map(|enemy| (
 					enemytypes.get(enemy.0.as_str()).unwrap().clone(),
-					enemy.1.to_vec2()
+					enemy.1
 				))
 				.collect()
 		}
