6030b3bc0c784fdcf0d1ad622e0632a8b8260b22
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-11-07 20:30:44 -0500

    Multithreaded texture loading
    I have no clue if this makes any sort of difference at all

--- src/gameplay/cores/textures.rs
+++ src/gameplay/cores/textures.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, thread};
 
 use image::{DynamicImage, ImageReader};
 
@@ -8,14 +8,22 @@ use super::{gen_name, get_files};
 pub fn get_textures() -> HashMap<String, DynamicImage> {
 	let mut textures: HashMap<String, DynamicImage> = HashMap::new();
 
+	let mut texture_handles = Vec::new();
+	let mut names = Vec::new();
+
 	for i in get_files(String::from("sprites")) {
-		textures.insert(
-			gen_name(&i),
+		names.push(gen_name(&i));
+
+		texture_handles.push(thread::spawn(move || -> DynamicImage {
 			ImageReader::open(&i)
 				.unwrap()
 				.decode()
 				.unwrap()
-		);
+		}));
+	}
+
+	for i in texture_handles.into_iter().enumerate() {
+		textures.insert(names[i.0].clone(), i.1.join().unwrap());
 	}
 
 	return textures;
