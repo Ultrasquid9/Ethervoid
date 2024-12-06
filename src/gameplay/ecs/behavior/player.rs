5f7110eb8a8786e2a77196fe8197623f3f243eab
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-12-06 12:33:19 -0500

    Player cannot change direction while dashing

--- src/gameplay/ecs/behavior/player.rs
+++ src/gameplay/ecs/behavior/player.rs
@@ -20,6 +20,55 @@ pub fn player_behavior(
 	config: &Config,
 	current_map: &str
 ) {
+	let mut new_pos = Vec2::new(0., 0.); // The pos to be moved to 
+
+	if !behavior.is_dashing { switch_dir_from_input(config, obj) }
+	match obj.axis_vertical {
+		Axis::Positive => new_pos.y += 1.,
+		Axis::Negative => new_pos.y -= 1.,
+		Axis::None => ()
+	}
+	match obj.axis_horizontal {
+		Axis::Positive => new_pos.x += 1.,
+		Axis::Negative => new_pos.x -= 1.,
+		Axis::None => ()
+	}
+
+	// Dashing
+	if config.keymap.dash.is_down() 
+	&& behavior.dash_cooldown <= 0.
+	&& new_pos != Vec2::new(0., 0.) {
+		behavior.speed += 12.;
+		behavior.dash_cooldown += 70.;
+	} else if behavior.dash_cooldown > 0. {
+		if behavior.dash_cooldown > 55. {
+			behavior.is_dashing = true;
+			behavior.speed = 12.;
+		} else {
+			behavior.is_dashing = false;
+		}
+		behavior.dash_cooldown -= get_delta_time();
+	}
+
+	// Makes the player build up speed over time, rather than instantly starting at max speed
+	if behavior.speed < 3.5 && new_pos != Vec2::new(0., 0.) {
+		behavior.speed += behavior.speed / 6.;
+	}
+
+	// Makes the player slow down if their speed is high
+	if behavior.speed > 4.5 {
+		behavior.speed /= 1.5;
+	}
+
+	if new_pos == Vec2::new(0., 0.) {
+		behavior.speed = 1.0;
+	} else {
+		obj.update(new_pos.normalize() * behavior.speed * get_delta_time() + obj.pos);
+		obj.try_move(obj.target, current_map);
+	}
+}
+
+fn switch_dir_from_input(config: &Config, obj: &mut Obj) {
 	// Checks to see if both Up and Down are being held at the same time.
 	// If they are, sets the direction to move based upon the most recently pressed key. 
 	// Otherwise, sets the direction to move based upon the currently pressed key.
@@ -60,49 +109,5 @@ pub fn player_behavior(
 		obj.axis_horizontal = Axis::Positive;
 	} else {
 		obj.axis_horizontal = Axis::None;
-	}
-
-	let mut new_pos = Vec2::new(0., 0.); // The pos to be moved to 
-
-	match obj.axis_vertical {
-		Axis::Positive => new_pos.y += 1.,
-		Axis::Negative => new_pos.y -= 1.,
-		Axis::None => ()
-	}
-	match obj.axis_horizontal {
-		Axis::Positive => new_pos.x += 1.,
-		Axis::Negative => new_pos.x -= 1.,
-		Axis::None => ()
-	}
-
-	// Dashing
-	if config.keymap.dash.is_down() && behavior.dash_cooldown <= 0.{
-		behavior.speed += 12.;
-		behavior.dash_cooldown += 70.;
-	} else if behavior.dash_cooldown > 0. {
-		if behavior.dash_cooldown > 55. {
-			behavior.is_dashing = true;
-			behavior.speed = 12.;
-		} else {
-			behavior.is_dashing = false;
-		}
-		behavior.dash_cooldown -= get_delta_time();
-	}
-
-	// Makes the player build up speed over time, rather than instantly starting at max speed
-	if behavior.speed < 3.5 && new_pos != Vec2::new(0., 0.) {
-		behavior.speed += behavior.speed / 6.;
-	}
-
-	// Makes the player slow down if their speed is high
-	if behavior.speed > 4.5 {
-		behavior.speed /= 1.5;
-	}
-
-	if new_pos == Vec2::new(0., 0.) {
-		behavior.speed = 1.0;
-	} else {
-		obj.update(new_pos.normalize() * behavior.speed * get_delta_time() + obj.pos);
-		obj.try_move(obj.target, current_map);
 	}
 }
